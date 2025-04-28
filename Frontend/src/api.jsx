64d1684a2073c2d5e7255dbdfe84ad71833efdf5
ShineLine-data <beqa0701@gmail.com>
2025-04-28 16:34:48 +0000

    define the name of the backend container as the IP address of the baseURL

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,7 +1,9 @@
 
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    // baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "http://file-rouge-ci-cd-general-backend-1:8000/api/",
+
 });
 
 export const UseAxios = () => {
